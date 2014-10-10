f01fa43b74e185f8744f432255107445109afe96
Marcin Lawniczak <marcin.safmb@wp.pl>
2014-10-10 21:01:15 +0200

    Upgrades

---
+++ app/controllers/CodesController.php
@@ -0,0 +1,31 @@
+<?php
+
+class CodesController extends \BaseController {
+
+	public function process($code)
+	{
+        $code = Code::where('code', '=', $code)->first();
+
+        if(is_null($code) || $code->used == 1){
+            return Redirect::to('/');
+        } else {
+            $code->used = 1;
+            $code->used_time = Carbon::now();
+            $code->used_ip = Request::getClientIp();
+            $mobileDetect = new Mobile_Detect();
+            $code->used_useragent = $mobileDetect->getUserAgent();
+            $code->save();
+
+
+            $path = storage_path().'\memes\\'.Meme::find($code->meme_id)->filename;
+
+            // Get the image
+            $image = Image::make($path)->encode('data-url');
+
+            return View::make('meme')->withMeme($code->meme)->withImage($image);
+        }
+
+    }
+
+
+}
