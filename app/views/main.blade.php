e5250e533a82212382179e74d0c61210dd43bc19
Robsonwp <perzfilip@gmail.com>
2014-10-13 22:25:41 +0200

    bootstrap css i js

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,4 +1,8 @@
 <html>
+<head>
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
 <body>
 
 {{ Form::open(array('url' => '/r', 'method' => 'get')) }}
