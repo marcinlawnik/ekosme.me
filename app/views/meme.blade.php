1bad7e9b5c9eef3c5f49fe88ca0da0f59e33cf61
Robsonwp <perzfilip@gmail.com>
2014-10-21 18:02:53 +0200

    wyglad

--- app/views/meme.blade.php
+++ app/views/meme.blade.php
@@ -1,35 +1,31 @@
 <html>
-
 <head>
+<style>
+img {
+        background: lightgrey;
+        color: white;
+        border-radius: 1em;
+        padding: 2em;
+        position: absolute;
+        top: 60%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%) }
+P.blocktext {
+    margin-left: auto;
+    margin-right: auto;
+    width: 10em
+}
 
-    <style>
-    
-    IMG.displayed
-    {
-       display: block;
-       margin-left: auto;
-       margin-right: auto 
-      
-    }
-    
-    </style>
-    
+</style>
 <script type="text/javascript">
-
-
 </script>
-
+<link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<h3><P class="blocktext">{{{ $meme->name }}}<br /></P></h3>
+<img class="" title="{{ $meme->name }}" src="{{{ $image }}}"></a><br />
 
-Image: <img class="" title="{{ $meme->name }}" src="{{{ $image }}}"></a><br />
-
-Title: {{{ $meme->name }}}<br />
-
-Description: {{{ $meme->description }}}<br />
-
-
+ <P class="blocktext">{{{ $meme->description }}}<br /></P>
 </body>
 </html>
-
-
