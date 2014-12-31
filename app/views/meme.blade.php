905dd48429bf952ab1b2f5cd31c7c35d4c9e0cfb
Marcin Lawniczak <marcin.safmb@wp.pl>
2014-12-31 12:40:38 +0100

    Fix bug if no voting

--- app/views/meme.blade.php
+++ app/views/meme.blade.php
@@ -30,12 +30,14 @@
             </div>
             <div class="">
                 <img id="meme" class="img-responsive img-thumbnail" title="{{ $meme->name }}" src="{{{ $image }}}">
+                @if(isset($code))
                 <div class="panel-footer text-center">
 
                     <a href="{{ URL::to('vote/'.$code.'/1') }}" class="btn btn-success btn-lg" >Spoko</a>
                     <a href="{{ URL::to('vote/'.$code.'/0') }}" class="btn btn-danger btn-lg" >Suchar</a>
 
                 </div>
+                @endif
             </div>
         </div>
     </div>
