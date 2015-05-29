1718eca8391f023617570f140d3af50efdc5dc95
Marcin Ławniczak <marcin.safmb@wp.pl>
2015-05-29 17:47:23 +0200

    Update index.blade.php

--- app/views/hs/index.blade.php
+++ app/views/hs/index.blade.php
@@ -17,16 +17,8 @@
             Jeśli tu jesteś to chyba tak?<br/>
             Zarejestruj się i zmierz się ze znajomymi!</h2>
     <br/>
-        <a href="{{ URL::to('hs/rules')  }}">
-            <img src="http://imgup.pl/di/DMRN/zasady.png"/>
-        </a>
-        <a href="ranking.html">
-            <img src="http://imgup.pl/di/Q3Z6/ranking.png"/>
-        </a>
-        <a href="rejestracja.html">
-            <img src="http://imgup.pl/di/LNQB/rejestracja.png"/>
-        </a>
-        <img src="http://imgup.pl/di/CGMA/t-o.png">
+    <a class="twitter-timeline"  href="https://twitter.com/EKOSTurniejHS" data-widget-id="604312504324460544">Tweety użytkownika @EKOSTurniejHS </a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 
     @include('hs.footer')
     </div>
