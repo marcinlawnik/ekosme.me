81dd754be7fbbfbe56d49d4c83a4af9ed19d7806
Marcin Ławniczak <marcin.safmb@wp.pl>
2015-05-29 18:20:49 +0200

    Update register.blade.php

--- app/views/hs/register.blade.php
+++ app/views/hs/register.blade.php
@@ -14,7 +14,7 @@
         <img src="http://upload.wikimedia.org/wikipedia/en/1/1c/Hearthstone_Logo.png">
         <h1>Rejestracja</h1>
 
-        <iframe src="https://docs.google.com/a/ekos.edu.pl/forms/d/1JhSQILo-RCVIbonbPVYQirNhI7UWQvi8C1i5tJZi7e8/viewform?embedded=true" width="100%" height="75%" frameborder="0" marginheight="0" marginwidth="0">Ładowanie...</iframe>
+        <iframe src="https://docs.google.com/a/ekos.edu.pl/forms/d/1JhSQILo-RCVIbonbPVYQirNhI7UWQvi8C1i5tJZi7e8/viewform?embedded=true" width="100%" height="700px" frameborder="0" marginheight="0" marginwidth="0">Ładowanie...</iframe>
 
         @include('hs.footer')
     </div>
