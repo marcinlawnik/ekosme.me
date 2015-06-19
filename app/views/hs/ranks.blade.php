c8f4f32bda5f3e87cea579be7ba4856bf7105f3d
Marcin Lawniczak <marcin.safmb@wp.pl>
2015-06-19 11:01:26 +0200

    Update of rules, cosmetic changes ,added ranking

--- app/views/hs/ranks.blade.php
+++ app/views/hs/ranks.blade.php
@@ -14,7 +14,7 @@
     <div class="container">
         <img src="http://upload.wikimedia.org/wikipedia/en/1/1c/Hearthstone_Logo.png">
         <p>RANKING</p>
-            <iframe src="https://docs.google.com/spreadsheets/d/1tDrspJY6s_0eCeLfocPVH77MgqDei8XKdKcH-SQ8tsE/pubhtml?gid=719055177&single=true&widget=true&headers=false"></iframe>
+            <iframe width="100%" height="700px" src="https://docs.google.com/spreadsheets/d/1tDrspJY6s_0eCeLfocPVH77MgqDei8XKdKcH-SQ8tsE/pubhtml?gid=719055177&single=true&widget=true&headers=false"></iframe>
         <hr>
         @include('hs.footer')
     </div>
