c64af752809ec0fb85942efe34eecdc6f102c8ab
Marcin Lawniczak <marcin.safmb@wp.pl>
2015-09-29 19:56:08 +0200

    Remove ads code

--- app/views/meme.blade.php
+++ app/views/meme.blade.php
@@ -38,18 +38,6 @@
 
                 </div>
                 @endif
-                <div>
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- meme page ekosme.me -->
-                    <ins class="adsbygoogle"
-                         style="display:block"
-                         data-ad-client="ca-pub-6563463508590145"
-                         data-ad-slot="8656018915"
-                         data-ad-format="auto"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
             </div>
         </div>
     </div>
