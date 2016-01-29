b0544551d0ec76662450d2494709a3cf5b1aa82f
Marcin Lawniczak <marcin.safmb@wp.pl>
2016-01-29 16:05:26 +0100

    Update mustknow

--- app/views/static/mustknow.blade.php
+++ app/views/static/mustknow.blade.php
@@ -39,7 +39,7 @@
                 <a href="https://ekosme.me/images/mustknow1.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
                     <img src="https://ekosme.me/images/mustknow1.png" class="img-responsive">
                 </a>
-                <a href="https://ekosme.me/images/mustknow_placeholder.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
+                <a href="https://ekosme.me/images/mustknow2.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
                     <img src="https://ekosme.me/images/mustknow2.png" class="img-responsive">
                 </a>
                 <a href="https://ekosme.me/images/mustknow_placeholder.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
