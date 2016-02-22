6bd0356aecb66f5dc5b5fdaa59428522699d8e56
Marcin Lawniczak <marcin.safmb@wp.pl>
2016-02-22 19:57:39 +0100

    Add mustknow 7 and another row of images

--- app/views/static/mustknow.blade.php
+++ app/views/static/mustknow.blade.php
@@ -59,6 +59,17 @@
                 </a>
             </div>
             <div class="row">
+                <a href="https://ekosme.me/images/mustknow7.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
+                    <img src="https://ekosme.me/images/mustknow7.png" class="img-responsive">
+                </a>
+                <a href="https://ekosme.me/images/mustknow_placeholder.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
+                    <img src="https://ekosme.me/images/mustknow_placeholder.png" class="img-responsive">
+                </a>
+                <a href="https://ekosme.me/images/mustknow_placeholder.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
+                    <img src="https://ekosme.me/images/mustknow_placeholder.png" class="img-responsive">
+                </a>
+            </div>
+            <div class="row">
                 <a href="https://ekosme.me/images/mustknow_placeholder.png" data-toggle="lightbox" data-gallery="memes" class="col-sm-4">
                     <img src="https://ekosme.me/images/mustknow_placeholder.png" class="img-responsive">
                 </a>
