ecb94f52e62089e8df9741d4881bac9e57addba5
Robsonwp <perzfilip@gmail.com>
2016-03-23 23:13:51 +0100

    row dla wszystkeigo

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -51,7 +51,6 @@
             </div>
             {{ Form::close() }}
         </div>
-        </div>
         <div class="container-fluid">
             {{-- top row of buttons --}}
             <div id="button_container" class="row row-centered">
