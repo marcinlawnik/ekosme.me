bd5b45791f567fec57f7a362aef6458df1102637
Robsonwp <perzfilip@gmail.com>
2016-03-24 13:57:01 +0100

    style dyrko.img bug fixed

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('head')
 <style>
-#imaginary_container
+#dyrkoimg
 {
     margin-top: 15%;
 }
-div.responsive-img
+#logo
 {
     display: block;
     margin-left: auto;
@@ -44,7 +44,7 @@ div#formsend{
 
 <div class="row">
     <div class="col s4 offset-s4">
-        <div class="responsive-img">
+        <div class="responsive-img" id="dyrkoimg">
         {{ HTML::image('logo_small.png', 'logo', ['id' => 'logo']) }}
     </div>
     </div>
