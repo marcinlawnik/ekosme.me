69837b6aa68f24800cb73449350733958c6f2c8c
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2016-06-13 20:14:27 +0200

    Kilka przeróbek na raport 2015/16
    zmiana roku i zakomentowanie podziękowań

--- app/views/reports/2014-2015/textBefore.blade.php
+++ app/views/reports/2014-2015/textBefore.blade.php
@@ -7,7 +7,7 @@
                 <img src="/images/logo.png" class="logo">
                 <h1 class="logo_text">ekosme.me</h1>
                 <br>
-                <p class="title">Raport Końcoworoczny 2014/2015</p>
+                <p class="title">Raport Końcoworoczny 2015/2016</p>
                 <p class="centered">
                     Dla: {{ $subscriber->email }}
                 </p>
@@ -52,38 +52,38 @@
 {{-- Page break for PDF --}}
 <div class="page-break"></div>
 
-<div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-        <div class="jumbotron">
-            <div class="page-header">
-                <h1 class="title">
-                    Podziękowania
-                </h1>
-            </div>
-            <div class="">
-                <p class="centered">
-                    Poniżej znajduje się lista osób, którym chciałbym serdecznie podziękować za pomoc. Kolejność alfabetyczna.
-                </p>
-                <table class="table table-bordered centered">
-                    <tr><td>Filip Perz</td></tr>
-                    <tr><td>Jacek Górecki</td></tr>
-                    <tr><td>Jakub Walczak</td></tr>
-                    <tr><td>Jan Śliwiński</td></tr>
-                    <tr><td>Karol Małecki</td></tr>
-                    <tr><td>Krzysztof Frydryk</td></tr>
-                    <tr><td>Krzysztof Tarociński</td></tr>
-                    <tr><td>Kuba Marszałkiewicz</td></tr>
-                    <tr><td>Mateusz Pliszka</td></tr>
-                    <tr><td>Michał Roszak</td></tr>
-                    <tr><td>Mikołaj Spiżewski</td></tr>
-                    <tr><td>Tymoteusz Ponikierski</td></tr>
-                    <tr><td>Wioletta Michałek</td></tr>
-                    <tr><td>Zuzanna Pelczyńska</td></tr>
-                </table>
-
-            </div>
-        </div>
-    </div>
-</div>
+//<div class="row">
+//    <div class="col-sm-10 col-sm-offset-1">
+//        <div class="jumbotron">
+//            <div class="page-header">
+//                <h1 class="title">
+//                    Podziękowania
+//                </h1>
+//            </div>
+//            <div class="">
+//                <p class="centered">
+//                    Poniżej znajduje się lista osób, którym chciałbym serdecznie podziękować za pomoc. Kolejność alfabetyczna.
+//                </p>
+//                <table class="table table-bordered centered">
+//                    <tr><td>Filip Perz</td></tr>
+//                    <tr><td>Jacek Górecki</td></tr>
+//                    <tr><td>Jakub Walczak</td></tr>
+//                    <tr><td>Jan Śliwiński</td></tr>
+//                    <tr><td>Karol Małecki</td></tr>
+//                    <tr><td>Krzysztof Frydryk</td></tr>
+//                    <tr><td>Krzysztof Tarociński</td></tr>
+//                    <tr><td>Kuba Marszałkiewicz</td></tr>
+//                    <tr><td>Mateusz Pliszka</td></tr>
+//                    <tr><td>Michał Roszak</td></tr>
+//                    <tr><td>Mikołaj Spiżewski</td></tr>
+//                    <tr><td>Tymoteusz Ponikierski</td></tr>
+//                    <tr><td>Wioletta Michałek</td></tr>
+//                    <tr><td>Zuzanna Pelczyńska</td></tr>
+//                </table>
+//
+//            </div>
+//        </div>
+//    </div>
+//</div>
 {{-- Page break for PDF --}}
 <div class="page-break"></div>
