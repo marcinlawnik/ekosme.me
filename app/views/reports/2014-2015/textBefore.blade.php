fa49e476ec56856d2800ec69112f5b395b17f375
kubamarszalkiewicz <kuba_marszalkiewicz@ekos.edu.pl>
2016-06-13 20:44:41 +0200

    zmiana na 2015/16
    słowo wstępne

--- app/views/reports/2014-2015/textBefore.blade.php
+++ app/views/reports/2014-2015/textBefore.blade.php
@@ -28,22 +28,20 @@
             </div>
             <div class="">
                 <p class="justified">
-                    Kolejny rok szkolny, 2014/2015 za nami. Od 5 listopada prowadzę ekosme.me, portal ze śmiesznymi (lub też nie) obrazkami związanymi z EKOSem.
-                    Pierwsza wersja powstała w ciągu 5 dni. Stwierdziłem, że warto zrealizować akurat ten pomysł, i się nie zawiodłem.
+                    Rok szkolny 2015/16 już się skończył. Nareszcie nastały wakacje. Warto sobie w tym czasie powspominać co się w ym roku działo.
+                    Może ten raport będzie dobrą podstawą do tego.
+                    Znajdziesz tutaj wszystkie memy z tego roku szkolnego oraz statystykę "spoko" i "sucharów" (a takżę twój głos). 
                 </p>
                 <p class="justified">
-                    W związku z tym, że udało się zebrać pieniądze na przedłużenie domeny na kolejny rok, przez taki właśnie czas co najmniej będę prowadził serwis.
-                    W ciągu wakacji pracuję nad nową funkcjonalnością, aby strona lepiej spełniała oczekiwania użytkowników i była bardziej przejrzysta.
+                    Ekosme.me na czas wakacji nie będzie wysyłać memów. Jednak będzie otwarte na ich propozycje. Więc jeśli najdize cię wizja - wysyłaj śmiało.
+                    Memy powrócą wraz z rozpoczęciem nowego roku szkolnego.
                 </p>
                 <p class="justified">
-                    Postaram się, aby w roku szkolnym 2015/2016 memy ukazywały się co 2-3 dni. Może być to trudne, ponieważ w EKOSie chyba aż tyle się nie dzieje, ale będę się starał.
-                    W tym celu nawiązałem współpracę z kilkoma uczniami naszej szkoły, którzy będą pomagać w prowadzeniu ekosme.me.
-                    Poniżej zamieszczone sa podziękowania, a także wszystkie memy z tego sezonu łącznie z informacją, jaką popularnością się cieszyły.
-                    Miłego oglądania!
+                    Nie zostało mi nic innego, jak podziękować za darowiznę na utrzymanie tego serwisu, życzyć dobrze przeżytych wakacji i miłego oglądania. 
                 </p>
                 <br>
                 <p class="signature">
-                    Marcin Ławniczak
+                    Kuba Marszałkiewicz
                 </p>
             </div>
         </div>
@@ -52,38 +50,38 @@
 {{-- Page break for PDF --}}
 <div class="page-break"></div>
 
-//<div class="row">
-//    <div class="col-sm-10 col-sm-offset-1">
-//        <div class="jumbotron">
-//            <div class="page-header">
-//                <h1 class="title">
-//                    Podziękowania
-//                </h1>
-//            </div>
-//            <div class="">
-//                <p class="centered">
-//                    Poniżej znajduje się lista osób, którym chciałbym serdecznie podziękować za pomoc. Kolejność alfabetyczna.
-//                </p>
-//                <table class="table table-bordered centered">
-//                    <tr><td>Filip Perz</td></tr>
-//                    <tr><td>Jacek Górecki</td></tr>
-//                    <tr><td>Jakub Walczak</td></tr>
-//                    <tr><td>Jan Śliwiński</td></tr>
-//                    <tr><td>Karol Małecki</td></tr>
-//                    <tr><td>Krzysztof Frydryk</td></tr>
-//                    <tr><td>Krzysztof Tarociński</td></tr>
-//                    <tr><td>Kuba Marszałkiewicz</td></tr>
-//                    <tr><td>Mateusz Pliszka</td></tr>
-//                    <tr><td>Michał Roszak</td></tr>
-//                    <tr><td>Mikołaj Spiżewski</td></tr>
-//                    <tr><td>Tymoteusz Ponikierski</td></tr>
-//                    <tr><td>Wioletta Michałek</td></tr>
-//                    <tr><td>Zuzanna Pelczyńska</td></tr>
-//                </table>
-//
-//            </div>
-//        </div>
-//    </div>
-//</div>
+<!--<div class="row">
+    <div class="col-sm-10 col-sm-offset-1">
+        <div class="jumbotron">
+            <div class="page-header">
+                <h1 class="title">
+                    Podziękowania
+                </h1>
+            </div>
+            <div class="">
+                <p class="centered">
+                    Poniżej znajduje się lista osób, którym chciałbym serdecznie podziękować za pomoc. Kolejność alfabetyczna.
+                </p>
+                <table class="table table-bordered centered">
+                    <tr><td>Filip Perz</td></tr>
+                    <tr><td>Jacek Górecki</td></tr>
+                    <tr><td>Jakub Walczak</td></tr>
+                    <tr><td>Jan Śliwiński</td></tr>
+                    <tr><td>Karol Małecki</td></tr>
+                    <tr><td>Krzysztof Frydryk</td></tr>
+                    <tr><td>Krzysztof Tarociński</td></tr>
+                    <tr><td>Kuba Marszałkiewicz</td></tr>
+                    <tr><td>Mateusz Pliszka</td></tr>
+                    <tr><td>Michał Roszak</td></tr>
+                    <tr><td>Mikołaj Spiżewski</td></tr>
+                    <tr><td>Tymoteusz Ponikierski</td></tr>
+                    <tr><td>Wioletta Michałek</td></tr>
+                    <tr><td>Zuzanna Pelczyńska</td></tr>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>-->
 {{-- Page break for PDF --}}
 <div class="page-break"></div>
