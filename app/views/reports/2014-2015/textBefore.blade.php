f728de77adf7d679a51491c22f94a4f530921279
Marcin Lawniczak <marcin.safmb@wp.pl>
2016-06-15 17:55:35 +0200

    Update readme and spelling fixes in report

--- app/views/reports/2014-2015/textBefore.blade.php
+++ app/views/reports/2014-2015/textBefore.blade.php
@@ -28,16 +28,16 @@
             </div>
             <div class="">
                 <p class="justified">
-                    Rok szkolny 2015/16 już się skończył. Nareszcie nastały wakacje. Warto sobie w tym czasie powspominać co się w ym roku działo.
+                    Rok szkolny 2015/16 już się skończył. Nareszcie nastały wakacje. Warto sobie w tym czasie powspominać co się w tym roku działo.
                     Może ten raport będzie dobrą podstawą do tego.
-                    Znajdziesz tutaj wszystkie memy z tego roku szkolnego oraz statystykę "spoko" i "sucharów" (a takżę twój głos). 
+                    Znajdziesz tutaj wszystkie memy z tego roku szkolnego oraz statystykę "spoko" i "sucharów" (a także twój głos).
                 </p>
                 <p class="justified">
-                    Ekosme.me na czas wakacji nie będzie wysyłać memów. Jednak będzie otwarte na ich propozycje. Więc jeśli najdize cię wizja - wysyłaj śmiało.
+                    Ekosme.me podczas wakacji nie będzie wysyłać memów. Będzie jednak otwarte na ich propozycje. Więc jeśli masz jakiś pomysł - wysyłaj śmiało.
                     Memy powrócą wraz z rozpoczęciem nowego roku szkolnego.
                 </p>
                 <p class="justified">
-                    Nie zostało mi nic innego, jak podziękować za darowiznę na utrzymanie tego serwisu, życzyć dobrze przeżytych wakacji i miłego oglądania. 
+                    Nie pozostało mi nic innego, jak podziękować za darowiznę na utrzymanie tego serwisu, życzyć dobrze przeżytych wakacji oraz miłego oglądania.
                 </p>
                 <br>
                 <p class="signature">
@@ -47,41 +47,5 @@
         </div>
     </div>
 </div>
-{{-- Page break for PDF --}}
-<div class="page-break"></div>
-
-<!--<div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-        <div class="jumbotron">
-            <div class="page-header">
-                <h1 class="title">
-                    Podziękowania
-                </h1>
-            </div>
-            <div class="">
-                <p class="centered">
-                    Poniżej znajduje się lista osób, którym chciałbym serdecznie podziękować za pomoc. Kolejność alfabetyczna.
-                </p>
-                <table class="table table-bordered centered">
-                    <tr><td>Filip Perz</td></tr>
-                    <tr><td>Jacek Górecki</td></tr>
-                    <tr><td>Jakub Walczak</td></tr>
-                    <tr><td>Jan Śliwiński</td></tr>
-                    <tr><td>Karol Małecki</td></tr>
-                    <tr><td>Krzysztof Frydryk</td></tr>
-                    <tr><td>Krzysztof Tarociński</td></tr>
-                    <tr><td>Kuba Marszałkiewicz</td></tr>
-                    <tr><td>Mateusz Pliszka</td></tr>
-                    <tr><td>Michał Roszak</td></tr>
-                    <tr><td>Mikołaj Spiżewski</td></tr>
-                    <tr><td>Tymoteusz Ponikierski</td></tr>
-                    <tr><td>Wioletta Michałek</td></tr>
-                    <tr><td>Zuzanna Pelczyńska</td></tr>
-                </table>
-
-            </div>
-        </div>
-    </div>
-</div>-->
 {{-- Page break for PDF --}}
 <div class="page-break"></div>
--- readme.md
+++ readme.md
@@ -1,20 +1,35 @@
 A meme website with subscribers.
-
 This will be the docs for when I forget stuff.
 Increasing the bus factor.
 
 Fixing permissions for /storage
 
-http://stackoverflow.com/a/22192814
+    http://stackoverflow.com/a/22192814
 
 I think chmod -777 is a bad practice.
 
 To solve permissions issue on Laravel, I've done this (with root user):
-cd app/
-chown -R www-data:www-data storage
-cd storage/
-find . -type d -exec chmod 775 {} \; && find . -type f -exec chmod 664 {} \;
-And always in app/storage directory :
-chown your_user:user_group .gitignore cache/.gitignore logs/.gitignore meta/.gitignore sessions/.gitignore views/.gitignore
+
+    cd app/
+    chown -R www-data:www-data storage
+    cd storage/
+    find . -type d -exec chmod 775 {} \; && find . -type f -exec chmod 664 {} \;
+    And always in app/storage directory :
+    chown your_user:user_group .gitignore cache/.gitignore logs/.gitignore meta/.gitignore sessions/.gitignore views/.gitignore
+
 Now, exit the root user, and it's ok.
 EDIT : this was for Laravel4. This doesn't works for Laravel5 because of a different structure.
+
+
+Finding what apache is running as
+    ps aux | egrep '(apache|httpd)'
+
+Xdebug tracing
+
+    php -d xdebug.auto_trace=ON -d xdebug.trace_output_dir=mytracedir/ myscript.php
+
+DB transfer
+
+    http://dubbs.github.io/blog/2013/09/05/mysql-import-slash-export-progress-bar/
+
+the mysql and mysqldump -h option allow to specify remote server
