3da97b5505deb8cf365527a5d4f7da8a734d952e
Xiankowaty01 <kuba_marszalkiewicz@ekos.edu.pl>
2017-02-28 09:29:48 +0100

    year changed (#43)
    2016 -> 2017

--- app/views/hs/footer.blade.php
+++ app/views/hs/footer.blade.php
@@ -1,5 +1,5 @@
 <hr>
 <!-- Site footer -->
 <footer class="footer">
-    <p>&copy; Marcin Ławniczak, Kuba Marszałkiewicz, Michał Roszak 2015-2016</p>
+    <p>&copy; Marcin Ławniczak, Kuba Marszałkiewicz, Michał Roszak 2015-2017</p>
 </footer>
